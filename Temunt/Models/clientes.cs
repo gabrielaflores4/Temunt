ccab2604bbea817e734391d4c7eab201462674c1
Douglas Moran <douglas.moran@catolica.edu.sv>
2025-10-01 00:10:03 -0600

    se modifico la clase publica clientes a clase publica Cliente

--- Temunt/Models/clientes.cs
+++ Temunt/Models/clientes.cs
@@ -1,6 +1,34 @@
-﻿namespace Temunt.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Temunt.Models
 {
-    public class clientes
+    [Table("clientes")]
+    public class Cliente
     {
+        [Key]
+        [Column("id_cliente")]
+        public int IdCliente { get; set; }
+
+        [Column("nombre")]
+        [Required] 
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Column("empresa")]
+        [StringLength(100)]
+        public string Empresa { get; set; }
+
+        [Column("contacto")]
+        [StringLength(100)]
+        public string Contacto { get; set; }
+
+        [Column("telefono")]
+        [StringLength(20)]
+        public string Telefono { get; set; }
+
+        [Column("email")]
+        [StringLength(100)]
+        public string Email { get; set; }
     }
 }
