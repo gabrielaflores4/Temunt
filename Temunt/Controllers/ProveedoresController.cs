55a1f5d9441f40faa226beb72f02f55d1026ded4
Juan José <juanjosemagaa@hotmail.com>
2025-09-30 23:07:48 -0600

    Creacion de pagina Index para proveedores junto a la creacion del css correspondiente y su controlador

---
+++ Temunt/Controllers/ProveedoresController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Temunt.Models;
+
+namespace Temunt.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        private readonly TemuntDbContext _context;
+
+        public ProveedoresController(TemuntDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var lista = _context.proveedores.ToList();  // <-- Aquí haces el READ
+            return View(lista);
+        }
+    }
+}
