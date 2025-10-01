e683b168cbc7eeeee437dfa57866680338f1529e
Douglas Moran <douglas.moran@catolica.edu.sv>
2025-10-01 00:08:47 -0600

    se agrego la conexion a la tabla clientes

--- Temunt/Models/TemuntDBContext.cs
+++ Temunt/Models/TemuntDBContext.cs
@@ -8,6 +8,8 @@ namespace Temunt.Models
     {
         public TemuntDbContext(DbContextOptions<TemuntDbContext> options) : base(options) { }
 
+        public DbSet<Cliente> Clientes { get; set; }
+
         public DbSet<usuarios> usuarios { get; set; }
         public DbSet<categorias> categorias { get; set; }
         public DbSet<pedidos> pedidos { get; set; }
