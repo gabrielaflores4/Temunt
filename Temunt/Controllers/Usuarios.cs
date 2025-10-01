f89c7b38254498bd6efec1a79f8e98341b4be959
AndreaClim <andrea.climaco@catolica.edu.sv>
2025-10-01 00:34:53 -0600

    Commit 5: Visualización de los Clientes en la Interfaz

--- Temunt/Controllers/Usuarios.cs
+++ Temunt/Controllers/Usuarios.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Temunt.Models;
 
 namespace Temunt.Controllers
 {
-    public class Usuarios : Controller
+    public class UsuariosController : Controller
     {
-        public IActionResult Index()
+        private readonly TemuntDbContext _context;
+
+        public UsuariosController(TemuntDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> ListaUsuarios()
+        {
+            var lista = await _context.usuarios.ToListAsync();
+            return View(lista); 
+        }
+
+        public IActionResult Create()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(usuarios usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(ListaUsuarios));
+            }
+            return View(usuario);
         }
     }
 }
