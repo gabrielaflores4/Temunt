2ba616a8a27199e515e9fd7ad8ea0ba7636433c4
Douglas Moran <douglas.moran@catolica.edu.sv>
2025-10-17 17:17:35 -0600

    se actualizo los modelos detalleP y pedidos

--- Temunt/Models/pedidos.cs
+++ Temunt/Models/pedidos.cs
@@ -5,19 +5,28 @@ namespace Temunt.Models
 {
     public class pedidos
     {
-
-
         [Key]
         public int id_pedidos { get; set; }
-        public DateTime fechaP { get; set; }
+        public DateTime? fechaP { get; set; }
         public int id_usuario { get; set; }
         public int id_estado { get; set; }
+        public int id_cliente { get; set; }
 
         [ForeignKey("id_usuario")]
         public virtual usuarios usuarios { get; set; }
 
         [ForeignKey("id_estado")]
         public virtual estadoP estadoP { get; set; }
+
+        [ForeignKey("id_cliente")]
+        public virtual Cliente clientes { get; set; }
+
+        public virtual ICollection<detalleP> detalleP { get; set; }
+
+        public decimal Monto => detalleP?.Sum(d => d.cantidad) ?? 0;
+
+        [NotMapped]
+        public decimal Total => detalleP?.Sum(d => d.cantidad * d.producto.precio) ?? 0;
 
     }
 }
