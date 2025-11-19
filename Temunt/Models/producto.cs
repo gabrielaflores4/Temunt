3c0e90b9c1b1f9cb60270104facd5ef424d36f71
Usuario <Usuario@DESKTOP-OVN89B6>
2025-11-19 15:01:35 -0600

    Funcion crear productos

--- Temunt/Models/producto.cs
+++ Temunt/Models/producto.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Temunt.Models
 {
@@ -15,9 +16,11 @@ namespace Temunt.Models
         public int id_prov { get; set; }
 
         [ForeignKey("id_cat")]
-        public virtual categorias categorias { get; set; }
+        [ValidateNever]
+        public categorias? categorias { get; set; }
 
         [ForeignKey("id_prov")]
-        public virtual proveedores proveedores { get; set; }
+        [ValidateNever]
+        public proveedores? proveedores { get; set; }
     }
 }
